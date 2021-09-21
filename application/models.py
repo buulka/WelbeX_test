f8620d70200478a38d39d45c246d9acea82cd4a3
Ekaterina Ivanova <katenika158@gmail.com>
2021-09-21 12:54:16 +0300

    Initial commit

---
+++ application/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField(blank=False, null=False)
+    name = models.TextField(blank=False, null=False)
+    count = models.IntegerField(blank=False, null=False)
+    distance = models.FloatField(blank=False, null=False)
+
+    def _str_(self):
+        return self.name
