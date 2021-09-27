daa84988499a8365f137646e6d09436c516474b9
Ekaterina Ivanova <katenika158@gmail.com>
2021-09-28 02:27:25 +0300

    Add settings for Heroku

--- WelbeX_test/settings.py
+++ WelbeX_test/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,7 +153,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#
-# GRAPHENE = {
-#     "SCHEMA": "application.schema.schema",
-# }
+django_heroku.settings(locals())
